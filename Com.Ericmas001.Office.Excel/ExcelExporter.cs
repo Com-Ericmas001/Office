54f3f0282543ac389118359b83176a4e0a843f2b
Éric Massé <Ericmas001@gmail.com>
2016-06-11 22:10:01 -0400

    Excel

---
+++ Com.Ericmas001.Office.Excel/ExcelExporter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using Com.Ericmas001.Common;
+using DataTable = System.Data.DataTable;
+using ExcelApp = Microsoft.Office.Interop.Excel;
+
+namespace Com.Ericmas001.Office.Excel
+{
+    public class ExcelExporter
+    {
+        public event EventHandler<EventArgs<int>> ProgressUpdated = delegate { };
+        public event EventHandler<EventArgs<int>> ExportationStarted = delegate { };
+        public event EventHandler<EventArgs<bool>> ExportationEnded = delegate { };
+
+        public void ExportDataTable(DataTable table)
+        {
+            ExportationStarted(this, new EventArgs<int>(table.Rows.Count));
+            new Thread(new ThreadStart(delegate
+            {
+                try
+                {
+                    var excelApp = new ExcelApp.Application();
+                    var excelWorkbook = excelApp.Workbooks.Add();
+                    ExcelApp.Worksheet sheet = excelWorkbook.Worksheets.Add();
+                    foreach (var sh in excelWorkbook.Worksheets.Cast<ExcelApp.Worksheet>().Where(sh => sh != sheet))
+                        sh.Delete();
+                    sheet.Name = string.IsNullOrWhiteSpace(table.TableName) ? "Table" : table.TableName;
+                    var cols = table.Columns.OfType<DataColumn>().Select(dc => dc.ColumnName).ToArray();
+
+                    var excelHeaders = sheet.Range[sheet.Cells[1, 1], sheet.Cells[1, cols.Length]];
+                    excelHeaders.Value2 = cols;
+                    excelHeaders.Font.Bold = true;
+                    excelHeaders.Interior.Color = ColorTranslator.ToOle(Color.Black);
+                    excelHeaders.Font.Color = ColorTranslator.ToOle(Color.White);
+                    var i = 2;
+                    foreach (DataRow dr in table.Rows)
+                    {
+                        ProgressUpdated(this, new EventArgs<int>(i - 1));
+                        var data = new string[cols.Length];
+                        for (var j = 0; j < data.Length; ++j)
+                            data[j] = dr[j].ToString();
+                        var excelRow = sheet.Range[sheet.Cells[i, 1], sheet.Cells[i, cols.Length]];
+                        excelRow.Value2 = data;
+                        i++;
+                    }
+                    sheet.Application.ActiveWindow.SplitRow = 1;
+                    sheet.Application.ActiveWindow.FreezePanes = true;
+                    var firstRow = (ExcelApp.Range) sheet.Rows[1];
+                    firstRow.AutoFilter(1, Type.Missing, ExcelApp.XlAutoFilterOperator.xlAnd, Type.Missing, true);
+                    firstRow.EntireColumn.AutoFit();
+                    excelApp.Visible = true;
+                    ExportationEnded(this, new EventArgs<bool>(true));
+                }
+                catch
+                {
+                    ExportationEnded(this, new EventArgs<bool>(false));
+                }
+            })).Start();
+        }
+    }
+}
---
+++ Com.Ericmas001.Office.Excel/ExcelUtil.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+
+namespace Com.Ericmas001.Office.Excel
+{
+    public static class ExcelUtil
+    {
+        public static IEnumerable<string> GetSheetNames(string excelFile)
+        {
+            OleDbConnection objConn = null;
+            DataTable dt = null;
+
+            try
+            {
+                objConn = new OleDbConnection($"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={excelFile};Extended Properties=\"Excel 12.0 Xml;HDR=YES;\"");
+                objConn.Open();
+                dt = objConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                return dt == null ? null : (from DataRow row in dt.Rows select row["TABLE_NAME"].ToString() into name where name.EndsWith("$") select name.Remove(name.Length - 1)).ToList();
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                if (objConn != null)
+                {
+                    objConn.Close();
+                    objConn.Dispose();
+                }
+                dt?.Dispose();
+            }
+        }
+        public static DataTable GetSheetData(string excelFile, string sheetname)
+        {
+            try
+            {
+                var excelConnection = new OleDbConnection($"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={excelFile};Extended Properties=\"Excel 12.0 Xml;HDR=YES;\"");
+                excelConnection.Open();
+                
+                var dbCommand = new OleDbCommand($"SELECT * FROM [{sheetname}$]", excelConnection);
+                var dataAdapter = new OleDbDataAdapter(dbCommand);
+                var dTable = new DataTable();
+                dataAdapter.Fill(dTable);
+
+                dTable.Dispose();
+                dataAdapter.Dispose();
+                dbCommand.Dispose();
+                excelConnection.Close();
+                excelConnection.Dispose();
+
+                return dTable;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
